9d54919debd9e317a91071dafd26f4f2b4065d94
Susanna Kosic <susanna.kosic@gmail.com>
2022-06-16 14:38:13 +0200

    feat: showing variable inputs in rows

--- src/formArrayEngine/variablesFormArray.tsx
+++ src/formArrayEngine/variablesFormArray.tsx
@@ -17,67 +17,91 @@ export const VariablesFields = ({
   });
 
   const { stepNumber } = useFormStepNumber();
-  
+
   return (
-    <div>
+    <div className="container-lg">
       {stepNumber === 2 &&
         fields.map((item, k) => {
           return (
-            <div key={item.id} style={{ marginLeft: 20 }}>
-              <label>Variable Name</label>
+            <>
+              <div key={item.id} className="row ms-4">
+                <div className="col">
+                  <label className="form-label">Variable Name</label>
+                  <input
+                    {...register(
+                      `formDetails.${nestIndex}.variables.${k}.variableName`,
+                      {
+                        pattern: {
+                          value: /^[A-Za-z]+$/i,
+                          message: "numbers are not allowed",
+                        },
+                      }
+                    )}
+                    style={{ marginRight: "25px" }}
+                    className="form-control"
+                  />
+                </div>
+                <div className="col">
+                  <label className="form-label">Variable Value</label>
 
-              <input
-                {...register(
-                  `formDetails.${nestIndex}.variables.${k}.variableName`,
-                  {
-                    pattern: {
-                      value: /^[A-Za-z]+$/i,
-                      message: "numbers are not allowed"
-                    }
-                  }
-                )}
-                style={{ marginRight: "25px" }}
-              />
-              
-              <label>Variable Value</label>
+                  <input
+                    type={"number"}
+                    {...register(
+                      `formDetails.${nestIndex}.variables.${k}.variableValue`
+                    )}
+                    className="form-control"
+                  />
+                </div>
+                <div className="col">
+                  <label className="form-label">Variable Type</label>
 
-              <input
-                type={"number"}
-                {...register(
-                  `formDetails.${nestIndex}.variables.${k}.variableValue`
-                )}
-              />
-              <label>Variable Type</label>
+                  <select
+                    {...register(
+                      `formDetails.${nestIndex}.variables.${k}.variableType`
+                    )}
+                    class="form-select"
+                  >
+                    <option value="default" disabled>
+                      Choose variable type...
+                    </option>
+                    <option value={"amount"}>Amount</option>
+                    <option value={"percentage"}>Percentage (%)</option>
+                  </select>
+                </div>
+                <div className="col">
+                  <label className="form-label">Notes</label>
 
-              <select
-                {...register(
-                  `formDetails.${nestIndex}.variables.${k}.variableType`
-                )}
-              >
-                <option value="default" disabled>
-                  Choose variable type...
-                </option>
-                <option value={"amount"}>Amount</option>
-                <option value={"percentage"}>Percentage (%)</option>
-              </select>
-
-              <label>Notes</label>
-
-              <input
-                {...register(`formDetails.${nestIndex}.variables.${k}.notes`)}
-              />
-
-              <button type="button" onClick={() => remove(k)}>
-                Delete variable
-              </button>
-            </div>
+                  <input
+                    {...register(
+                      `formDetails.${nestIndex}.variables.${k}.notes`
+                    )}
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div className="row ms-4 mt-4">
+                <div className="col">
+                  <button
+                    type="button"
+                    onClick={() => remove(k)}
+                    className="align-baseline"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </>
           );
         })}
 
       {stepNumber === 2 && (
-        <button type="button" onClick={() => append({})}>
-          Add More Variables
-        </button>
+        <div className="row">
+          <div className="col-6">
+            <button className="mt-5" type="button" onClick={() => append({})}>
+              Add More Variables
+            </button>
+          </div>
+        </div>
       )}
 
       <hr />
