b5914ceab8f0e286217a7995ddc001c77d45080f
Susanna Kosic <susanna.kosic@gmail.com>
2022-06-17 06:47:26 +0200

    style: display headers on recap

--- src/Recap.tsx
+++ src/Recap.tsx
@@ -1,4 +1,4 @@
-import { arrayBuffer } from "stream/consumers";
+import Table from "react-bootstrap/Table";
 
 type RecapData = {
   monthlySalary: string;
@@ -14,57 +14,79 @@ export default function Recap({ data }: { data: RecapData }) {
           <h2>Recap</h2>
         </div>
       </div>
-      <div className="row mb-4">
+      <div className="row">
         <div className="col">
-          <p>
-            salary: {data.monthlySalary}
-            {` (${data.territory})`}
-          </p>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>salary</th>
+                <th>territory</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{data.monthlySalary}</td>
+                <td>{data.territory}</td>
+              </tr>
+            </tbody>
+          </Table>
         </div>
       </div>
       <div className="row">
         <div className="col">
           <h3>Variables</h3>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>variable name</th>
+                <th>variable type</th>
+                <th>variable value</th>
+                <th>notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.variables?.map((variable: any, index: number) => (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{variable.variableName}</td>
+                  <td>{variable.variableType}</td>
+                  <td>{variable.variableValue}</td>
+                  <td>{variable.notes}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </div>
       </div>
-      {data.variables?.map((variable: any) => (
-        <div className="row mb-4">
-          <div className="col">
-            <p>{variable.variableName}</p>
-          </div>
-          <div className="col">
-            <p>{variable.variableType}</p>
-          </div>
-          <div className="col">
-            <p>{variable.variableValue}</p>
-          </div>
-          <div className="col">
-            <p>{variable.notes}</p>
-          </div>
-        </div>
-      ))}
 
       <div className="row">
         <div className="col">
           <h3>Calculated costs</h3>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>calculation name</th>
+                <th>calculation type</th>
+                <th>calculation value</th>
+                <th>notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.calculations?.map((calculation: any, index: number) => (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{calculation.calculationName}</td>
+                  <td>{calculation.formula}</td>
+                  <td>{calculation.testResult}</td>
+                  <td>{calculation.notes}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </div>
       </div>
-      {data.calculations?.map((calculation: any) => (
-        <div className="row">
-          <div className="col">
-            <p>{calculation.calculationName}</p>
-          </div>
-          <div className="col">
-            <p>{calculation.formula}</p>
-          </div>
-          <div className="col">
-            <p>{calculation.testResult}</p>
-          </div>
-          <div className="col">
-            <p>{calculation.notes}</p>
-          </div>
-        </div>
-      ))}
     </div>
   );
 }
