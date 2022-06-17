05d4e953b591877f177f586ff8ca87911d2eb771
Susanna Kosic <susanna.kosic@gmail.com>
2022-06-17 06:01:17 +0200

    style: fix territory field alignment

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { useFormStepNumber } from "./hooks/useFormStepNumber";
 import { useState } from "react";
 import { Modal } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const defaultValues = {
   formDetails: [
@@ -55,111 +58,118 @@ function App() {
 
   console.log("data", data, "step", stepNumber);
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>OmniAtlas Calculator Engine</h1>
-        {errors && Array.isArray(errors.formDetails) && (
-          <span style={{ color: "red" }}>
-            {errors.formDetails[0].variables?.map(
-              (variable) => variable?.variableName?.message
+    <Container style={{ paddingTop: "10rem" }}>
+      <Row>
+        <Col>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>OmniAtlas Calculator Engine</h1>
+            {errors && Array.isArray(errors.formDetails) && (
+              <span style={{ color: "red" }}>
+                {errors.formDetails[0].variables?.map(
+                  (variable) => variable?.variableName?.message
+                )}
+              </span>
             )}
-          </span>
-        )}
-        {stepNumber < 4 && (
-          <FieldArray
-            {...{
-              control,
-              register,
-              defaultValues,
-              getValues,
-              setValue,
-            }}
-          />
-        )}
-        {(stepNumber === 4 || stepNumber === 5) && (
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h2>Recap</h2>
-              </div>
-            </div>
-            <div className="row mb-4">
-              <div className="col">
-                <p>salary: {data?.formDetails[0].monthlySalary}</p>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <h3>Variables</h3>
-              </div>
-            </div>
-            {data?.formDetails[0].variables?.map((variable: any) => (
-              <div className="row mb-4">
-                <div className="col">
-                  <p>{variable.variableName}</p>
+            {stepNumber < 4 && (
+              <FieldArray
+                {...{
+                  control,
+                  register,
+                  defaultValues,
+                  getValues,
+                  setValue,
+                }}
+              />
+            )}
+            {(stepNumber === 4 || stepNumber === 5) && (
+              <div className="container">
+                <div className="row">
+                  <div className="col">
+                    <h2>Recap</h2>
+                  </div>
                 </div>
-                <div className="col">
-                  <p>{variable.variableType}</p>
+                <div className="row mb-4">
+                  <div className="col">
+                    <p>
+                      salary: {data?.formDetails[0].monthlySalary}
+                      {`(${data?.formDetails[0].territory})`}
+                    </p>
+                  </div>
                 </div>
-                <div className="col">
-                  <p>{variable.variableValue}</p>
+                <div className="row">
+                  <div className="col">
+                    <h3>Variables</h3>
+                  </div>
                 </div>
-                <div className="col">
-                  <p>{variable.notes}</p>
-                </div>
-              </div>
-            ))}
+                {data?.formDetails[0].variables?.map((variable: any) => (
+                  <div className="row mb-4">
+                    <div className="col">
+                      <p>{variable.variableName}</p>
+                    </div>
+                    <div className="col">
+                      <p>{variable.variableType}</p>
+                    </div>
+                    <div className="col">
+                      <p>{variable.variableValue}</p>
+                    </div>
+                    <div className="col">
+                      <p>{variable.notes}</p>
+                    </div>
+                  </div>
+                ))}
 
-            <div className="row">
-              <div className="col">
-                <h3>Calculated costs</h3>
+                <div className="row">
+                  <div className="col">
+                    <h3>Calculated costs</h3>
+                  </div>
+                </div>
+                {data?.formDetails[0].calculations?.map((calculation: any) => (
+                  <div className="row">
+                    <div className="col">
+                      <p>{calculation.calculationName}</p>
+                    </div>
+                    <div className="col">
+                      <p>{calculation.formula}</p>
+                    </div>
+                    <div className="col">
+                      <p>{calculation.testResult}</p>
+                    </div>
+                    <div className="col">
+                      <p>{calculation.notes}</p>
+                    </div>
+                  </div>
+                ))}
               </div>
-            </div>
-            {data?.formDetails[0].calculations?.map((calculation: any) => (
-              <div className="row">
-                <div className="col">
-                  <p>{calculation.calculationName}</p>
-                </div>
-                <div className="col">
-                  <p>{calculation.formula}</p>
-                </div>
-                <div className="col">
-                  <p>{calculation.testResult}</p>
-                </div>
-                <div className="col">
-                  <p>{calculation.notes}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
+            )}
 
-        <Modal show={Boolean(stepNumber === 5)} centered size="sm">
-          {/* onHide={handleClose} */}
-          <Modal.Body>
-            <p style={{ color: "black" }}>
-              By saving this data it will be available on the OmniCalculator, so
-              can other use it. If you are sure about the results of your
-              calculations, you can proceed by saving.
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => setStepNumber(4)}>
-              Cancel
-            </Button>
-            <Button variant="primary" onClick={() => setStepNumber(6)}>
-              Save
-            </Button>
-          </Modal.Footer>
-        </Modal>
-        {stepNumber === 6 && (
-          <p className="mt-5 text-center">
-            Data has been saved on the OmniCalculator.
-          </p>
-        )}
-        {stepNumber < 6 && <input type="submit" value={"Continue"} />}
-      </form>
-    </div>
+            <Modal show={Boolean(stepNumber === 5)} centered size="sm">
+              {/* onHide={handleClose} */}
+              <Modal.Body>
+                <p style={{ color: "black" }}>
+                  By saving this data it will be available on the
+                  OmniCalculator, so can other use it. If you are sure about the
+                  results of your calculations, you can proceed by saving.
+                </p>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={() => setStepNumber(4)}>
+                  Cancel
+                </Button>
+                <Button variant="primary" onClick={() => setStepNumber(6)}>
+                  Save
+                </Button>
+              </Modal.Footer>
+            </Modal>
+            {stepNumber === 6 && (
+              <p className="mt-5 text-center">
+                Data has been saved on the OmniCalculator.
+              </p>
+            )}
+            {stepNumber < 6 && <input type="submit" value={"Continue"} />}
+          </form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
